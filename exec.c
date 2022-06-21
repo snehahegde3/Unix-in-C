1c80d1f8a0f8a5339cf86a19431147f76323e93e
Sneha Hegde <snehahegde33@gmail.com>
2022-06-21 22:40:38 +0530

    exec

--- exec.c
+++ exec.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//for windows, 
+// for windows,
 // #include <process.h>
-//for linux
+// for linux
 #include <unistd.h>
+#include <errno.h>
 
-int main(int argc, char *argv){
-    _execl(/*exe, path of exe, argument to exe, NULL*/);
+int main(int argc, char *argv, char *envp[])
+{
+    // exec overwrites everything not only memory, the entire execution
+
+    // once we call this we are no longer in our old program
+
+    // execl takes a list of arguements
+    //  execl("C:\\Windows\\System32\\ping.exe", "C:\\Windows\\System32\\ping.exe", "google.com", NULL);
+
+    // execlp uses path variables to find ping
+    // execlp("ping", "ping", "google.com", NULL);
+
+    // execvp takes an array
+    char *arr[] = {
+        "ping", "google.com", NULL};
+    // execvp("ping", arr);
+
+    // execvpe takes environement
+    char *env[] = {
+        "TEST=enivironment variable",
+        NULL};
+    execvpe("ping", arr, env);
+
+    // runs when ping fails
+    int err = errno;
+    printf("Ping finished executing\n");
+
     return 0;
 }
