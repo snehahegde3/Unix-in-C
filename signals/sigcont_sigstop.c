7b40aadcc7a59d7c6d541ffd4e2679a9919adff4
Sneha Hegde <snehahegde33@gmail.com>
2022-06-22 11:49:57 +0530

    sigcont & sigstop

---
+++ signals/sigcont_sigstop.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <time.h>
+#include <signal.h>
+
+int main(int argc, char *argv[])
+{
+    int pid = fork();
+    if (pid == -1)
+    {
+        printf("Fork failed\n");
+        return 1;
+    }
+
+    if (pid == 0)
+    {
+        while (1)
+        {
+            printf("output\n");
+            usleep(50000);
+        }
+    }
+
+    else
+    {
+        // stop before taking input
+        kill(pid, SIGSTOP);
+        int t;
+        do
+        {
+            // input time for execution
+            printf("Time in secs for execution: ");
+            scanf("%d", &t);
+            // if t is -ve it goes on for max_int time
+            if (t > 0)
+            {
+                // continues for t secs, stops
+                kill(pid, SIGCONT);
+                sleep(t);
+                kill(pid, SIGSTOP);
+            }
+
+        } while (t > 0);
+
+        kill(pid, SIGKILL);
+
+        wait(NULL);
+    }
+}
